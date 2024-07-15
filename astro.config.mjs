2841db2ab5e3782110a3d29106948498b9306bd5
Frenkie Wang <frenkiewang21@gmail.com>
2024-07-15 14:21:53 +0100

    Change astro.config.mjs and deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://TracyZZZmm.github.io/mywebsite/",
-  routes: [
-    { src: "/", dest: "/src/pages/index.astro" },
-    { src: "/about", dest: "/src/pages/about.astro" },
-    { src: "/contact", dest: "/src/pages/contact.astro" },
-  ],
+  site: "https://github.com/TracyZZZmm/TracyZZZmm.github.io"
 });
